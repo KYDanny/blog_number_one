c33cfc87e70527bf5753dde8f539033ecddfda64
KYDanny <chiou.kuan.yih@gmail.com>
2023-10-31 16:06:49 +0800

    add taggit to installed apps in settings.py, in Post models add tags field, add list.html with Tags, modify post_list def to include optional tag_slug and render tag to template, in blog/urls.py add new path named "post_list_by_tag", modify list.html with tags with urls

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,14 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
     try:
@@ -15,7 +20,7 @@ def post_list(request):
        posts = paginator.page(paginator.num_pages)
     except PageNotAnInteger:
        posts = paginator.page(1)
-    return render(request, 'blog/post/list.html', {'posts':posts})
+    return render(request, 'blog/post/list.html', {'posts':posts, 'tag':tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
