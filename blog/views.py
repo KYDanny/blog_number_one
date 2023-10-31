b7fd9cf7edb3af7935ad097b19cb6d8fdc017338
KYDanny <chiou.kuan.yih@gmail.com>
2023-10-31 19:18:55 +0800

    retrieve posts by similarity: edit post_detail views to create similar_posts, and edit detail.py to meet the needs

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 def post_list(request, tag_slug=None):
     post_list = Post.published.all()
@@ -31,7 +32,11 @@ def post_detail(request, year, month, day, post):
                              slug = post)
     comments = post.comments.filter(active=True)
     form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post':post, 'form':form, 'comments':comments})
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+    return render(request, 'blog/post/detail.html', {'post':post, 'form':form, 'comments':comments,
+                                                     'similar_posts':similar_posts})
 
 # class PostListView(ListView):
 #     queryset = Post.published.all()
